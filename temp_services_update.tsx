d58a006176b0a3427abb107853131b6d8dabfe6b
CryptoCOB <crypto.circle.of.bosses@gmail.com>
2025-12-17 17:46:42 -0500

    refactor: move PDFs and remove temp files from root; organize project structure

--- temp_services_update.tsx
+++
@@ -1,7 +0,0 @@
-// Professional icon for services
-const defaultIcon = <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
-  <path d="M12 2L2 7v10c0 5.55 3.84 9.64 9 11 5.16-1.36 9-5.45 9-11V7l-10-5z"/>
-  <path d="M8 12h8v2H8zm0-3h8v2H8z" fill="white"/>
-</svg>;
-
-// Replace all emoji strings with this defaultIcon
